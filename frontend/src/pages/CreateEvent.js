6b0dbed64a25cdc26ecbfeb702a233cae0a53b46
Balaji Gokul Krishna Sai <149820969+mFStarBoy483@users.noreply.github.com>
2025-02-10 08:25:59 +0530

    Update CreateEvent.js

--- frontend/src/pages/CreateEvent.js
+++ frontend/src/pages/CreateEvent.js
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './CreateEvent.css';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
-
+const API_URL = 'https://event-management-o3vq.onrender.com';
 const CreateEvent = () => {
   const [formData, setFormData] = useState({
     title: '',
